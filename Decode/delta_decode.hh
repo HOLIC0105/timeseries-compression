2b8becdef50c09294286f2b7c79eeb4052521828
HOLIC0105 <3485995896@qq.com>
2024-04-18 16:58:04 +0800

    update

--- Decode/delta_decode.hh
+++ Decode/delta_decode.hh
@@ -4,14 +4,12 @@
 #include <stddef.h>
 #include <vector>
 #include <iostream>
-
 template<typename T>
 void DeltaDecode(std::vector<T> &delta) {
   for(size_t i = 1; i < delta.size(); i ++) {
     delta[i] += delta[i - 1];
   }
 }
-
 template<typename T>
 void DeltaOfDeltaDecode(std::vector<T> &delta) {
  for(size_t i = 2; i < delta.size(); i ++) {
